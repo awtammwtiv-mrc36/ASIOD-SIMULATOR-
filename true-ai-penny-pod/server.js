9d34d1984d42d025daf393b01ae25144bc879c78
Jt Browne <awtammwtiv@gmail.com>
2026-05-29 15:31:34 +0100

    Update server.js
    Signed-off-by: Jt Browne  <awtammwtiv@gmail.com>

--- true-ai-penny-pod/server.js
+++ true-ai-penny-pod/server.js
@@ -3619,7 +3619,7 @@ function requireGeometryGate(req, res, next) {
   return next();
 }
 
-app.get('/geometry', requireGeometryGate, async (_req, res) => {
+app.get('/geometry', async (_req, res) => {
   const state = await pullGeometryRoot('page-request');
 
   if (state.cachedBody) {
