ed05b1d38d76323f7a1b7cc263a903ebbd086dd2
Jt Browne <awtammwtiv@gmail.com>
2026-06-03 20:27:16 +0100

    Import OpenAI in server.js
    Signed-off-by: Jt Browne  <awtammwtiv@gmail.com>

--- true-ai-penny-pod/server.js
+++ true-ai-penny-pod/server.js
@@ -1,3 +1,4 @@
+import OpenAI from 'openai';
 import crypto from 'crypto';
 import express from 'express';
 import Stripe from 'stripe';
@@ -7,6 +8,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const client = new OpenAI();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let HYBRID_ENGINE_WORKER_BRIDGE = Object.freeze({
